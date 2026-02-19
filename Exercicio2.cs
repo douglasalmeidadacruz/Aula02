abbcc01e3f388155510e4c9bf035806742ebd965
DOUGLAS ALMEIDA DA CRUZ <douglas.cruz2@portalsesisp.org.br>
2026-02-19 11:32:09 -0300

    Entregando Ex1, 2 e 3

---
+++ Exercicio2.cs
@@ -0,0 +1,12 @@
+// -- Arquivo Exercicio2.cs
+public static class Exercicio2
+{
+    //private static string nome = "Douglas Almeida";
+    
+    public static void Executar()
+    {
+        string nome = "Douglas Almeida";
+        Console.WriteLine(nome);
+        Console.WriteLine();
+    }
+}
